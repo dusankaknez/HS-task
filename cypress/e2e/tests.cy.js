f642dd706c685630b104187279a1b8e78d4b4f98
dusanka.knez@gmail.com <dusanka.knez@gmail.com>
2026-01-22 00:44:56 +0100

    Tests 1-5 finished as per the task

--- cypress/e2e/tests.cy.js
+++ cypress/e2e/tests.cy.js
@@ -8,23 +8,33 @@ beforeEach('Open test application', ()=>{
 
 })
 
-describe('Main Tests 1-3', ()=>{
+describe('Main Tests 1-5', ()=>{
 
-    it('Login as standard user and check url', () => {
+    it('1. Login as standard user and check url', () => {
         onLoginPage.loginUser('standard_user', 'secret_sauce')
         cy.url().should('include', '/inventory.html');
     })
-    it('Login as standard user, add to cart and validate remove button', () => {
+    it('2.Login as standard user, add to cart and validate remove button', () => {
         onLoginPage.loginUser('standard_user', 'secret_sauce')        
         cy.url().should('include', '/inventory.html');
-    
         onInventoryPage.addItemToCart('Sauce Labs Backpack')
     })
-    it('Login as problem user and validate remove image', () => {
+    // This test should fail
+    it('3.Login as problem user and validate image for item', () => {
         onLoginPage.loginUser('problem_user', 'secret_sauce')
         onInventoryPage.validateImageForItem('Sauce Labs Onesie','red-onesie')        
-        cy.url().should('include', '/inventory.html');
+        cy.url().should('include', '/inventory.html'); 
+    })
 
-    
+    it('4.Login as locked user and validate error', () => {
+        onLoginPage.loginUser('locked_out_user', 'secret_sauce')
+        cy.get('[data-test="error"]').should('have.text', 'Epic sadface: Sorry, this user has been locked out.');
+       
+    })
+    // This test should fail
+    it.only('5.Login as error user and validate error', () => {
+        onLoginPage.loginUser('error_user', 'secret_sauce')
+        onInventoryPage.addItemToCart('Sauce Labs Fleece Jacket')
+  
     })
 })
