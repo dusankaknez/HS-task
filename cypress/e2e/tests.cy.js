5928e998134235e5b22dd50ab6f1443fc396b278
dusanka.knez@gmail.com <dusanka.knez@gmail.com>
2026-01-22 02:09:00 +0100

    Some formatting changed

--- cypress/e2e/tests.cy.js
+++ cypress/e2e/tests.cy.js
@@ -8,8 +8,8 @@ beforeEach('Open test application', ()=>{
 
 })
 
-describe('Main Tests 1-5', ()=>{
-       const standardUser = Cypress.env('users').standard;
+describe('Core Test Scenarios', ()=>{
+    const standardUser = Cypress.env('users').standard;
     it('1. Login as standard user and check url', () => {
         onLoginPage.loginUser(standardUser.username, standardUser.password)
         cy.url().should('include', '/inventory.html');
@@ -19,20 +19,19 @@ describe('Main Tests 1-5', ()=>{
         cy.url().should('include', '/inventory.html');
         onInventoryPage.addItemToCart('Sauce Labs Backpack')
     })
-    // This test should fail
+    // This test will fail
     it('3.Login as problem user and validate image for item', () => {
         const problemUser = Cypress.env('users').problem;
         onLoginPage.loginUser(problemUser.username, problemUser.password)
         onInventoryPage.validateImageForItem('Sauce Labs Onesie','red-onesie')        
     })
-
     it('4.Login as locked user and validate error', () => {
         const lockedUser = Cypress.env('users').locked;
         onLoginPage.loginUser(lockedUser.username, lockedUser.password)
         cy.get('[data-test="error"]').should('have.text', 'Epic sadface: Sorry, this user has been locked out.');
        
     })
-    // This test should fail
+    // This test will fail
     it('5.Login as error user and validate error', () => {
         const errorUser = Cypress.env('users').error;
         onLoginPage.loginUser(errorUser.username, errorUser.password)
